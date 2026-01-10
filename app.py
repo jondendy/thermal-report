1c6d168ff327c5b0a1446ed6b4018b90aace4f28
jonAndy <81487636+jondendy@users.noreply.github.com>
2026-01-10 21:39:36 +0000

    Update app to use environment variable for port
    Change to use environment variable for port and disable debug mode.

--- app.py
+++ app.py
@@ -289,4 +289,6 @@ def server_error(error) -> tuple:
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=8080)
+    import os
+    port = int(os.environ.get("PORT", 8080))
+    app.run(debug=False, host="0.0.0.0", port=port)
