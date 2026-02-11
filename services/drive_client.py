df31aeedd2202a5aa069d47be833ef1aeb0713d4
jonAndy <81487636+jondendy@users.noreply.github.com>
2026-02-11 07:27:17 +0000

    Implement file listing and downloading functions
    Added functions to list files in a folder and download a file from Google Drive.
    
    dup. into this feature branch

--- services/drive_client.py
+++ services/drive_client.py
@@ -13,6 +13,19 @@ def get_drive_service():
     return build("drive", "v3", credentials=creds)
 
 def list_files_in_folder(folder_id):
+    service = get_drive_service()
+    q = f"'{folder_id}' in parents and trashed = false"
+    results = service.files().list(q=q, fields="files(id, name)").execute()
+    return results.get("files", [])
+
+def download_file(file_id, dest_path):
+    service = get_drive_service()
+    request = service.files().get_media(fileId=file_id)
+    with open(dest_path, "wb") as fh:
+        downloader = MediaIoBaseDownload(fh, request)
+        done = False
+        while not done:
+            status, done = downloader.next_chunk()
 
 def get_folder_metadata(folder_id):
     service = get_drive_service()
@@ -41,15 +54,3 @@ def rename_folder(folder_id, new_name):
     file_metadata = {'name': new_name}
     updated_file = service.files().update(fileId=folder_id, body=file_metadata, fields='id, name').execute()
     return updated_file
-    service = get_drive_service()
-    q = f"'{folder_id}' in parents and trashed = false"
-    results = service.files().list(q=q, fields="files(id, name)").execute()
-    return results.get("files", [])
-
-def download_file(file_id, dest_path):
-    service = get_drive_service()
-    request = service.files().get_media(fileId=file_id)
-    with open(dest_path, "wb") as fh:
-        downloader = MediaIoBaseDownload(fh, request)
-        done = False
-        _, done = downloader.next_chunk()
